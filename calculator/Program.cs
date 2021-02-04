d2159f9bd695003d4bbf73c9ef0c06c3c8adb63d
kenny-kogi <mwangiken497@gmail.com>
2021-02-04 13:36:25 +0300

    simple calc

---
+++ calculator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number 1: ");
+            Double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Operator: ");
+            string oper = Console.ReadLine();
+            Console.Write("Enter Number 2: ");
+            Double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (oper){
+                case "x":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "/":
+                    Console.WriteLine(num1 / num2);
+                    break;
+            }
+        }
+    }
+}
